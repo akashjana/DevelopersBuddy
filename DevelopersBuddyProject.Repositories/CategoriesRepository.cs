ead38a1059648c9881a2f1da3c062c7a9c61a2e8
Akash Jana <akashjana2030@gmail.com>
2023-02-21 20:08:00 +0530

    added user and category repository implementation

---
+++ DevelopersBuddyProject.Repositories/CategoriesRepository.cs
@@ -0,0 +1,63 @@
+﻿using DevelopersBuddyProject.DomainModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevelopersBuddyProject.Repositories
+{
+    public interface ICategoriesRepository
+    {
+        void InsertCategory(Category category);
+        void UpdateCategory(Category category);
+        void DeleteCategory(int categoryId);
+        List<Category> GetCategories();
+        List<Category> GetCategoryByCategoryId(int categoryId);
+    }
+    public class CategoriesRepository : ICategoriesRepository
+    {
+        readonly DevelopersBuddyDatabaseDbContext db;
+        public CategoriesRepository()
+        {
+            db = new DevelopersBuddyDatabaseDbContext();
+        }
+        public void DeleteCategory(int categoryId)
+        {
+            Category existingCategory = db.Categories.Where(x => x.CategoryId == categoryId).FirstOrDefault();
+            if (existingCategory != null)
+            {
+                db.Categories.Remove(existingCategory);
+                db.SaveChanges();
+            }
+        }
+
+        public List<Category> GetCategories()
+        {
+            List<Category> categories = db.Categories.ToList();
+            return categories;
+        }
+
+        public List<Category> GetCategoryByCategoryId(int categoryId)
+        {
+            List<Category> category = db.Categories.Where(x => x.CategoryId == categoryId).ToList();
+            return category;
+        }
+
+        public void InsertCategory(Category category)
+        {
+            db.Categories.Add(category);
+            db.SaveChanges();
+        }
+
+        public void UpdateCategory(Category category)
+        {
+            Category existingCategory = db.Categories.Where(x => x.CategoryId == category.CategoryId).FirstOrDefault();
+            if (existingCategory != null)
+            {
+                existingCategory.CategoryName = category.CategoryName;
+                db.SaveChanges();
+            }
+        }
+    }
+}
---
+++ DevelopersBuddyProject.Repositories/UsersRepository.cs
@@ -0,0 +1,97 @@
+﻿using DevelopersBuddyProject.DomainModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevelopersBuddyProject.Repositories
+{
+    public interface IUsersRepository
+    {
+        void InsertUser(User user);
+        void UpdateUserDetails(User user);
+        void UpdateUserPassword(User user);
+        void DeleteUser(int userId);
+        List<User> GetUsers();
+        List<User> GetUsersByEmailAndPassword(string email, string password);
+        List<User> GetUsersByEmail(string email);
+        List<User> GetUsersByUserId(int userId);
+        int GetLatestUserId();
+    }
+
+    public class UsersRepository : IUsersRepository
+    {
+        readonly DevelopersBuddyDatabaseDbContext db;
+        public UsersRepository()
+        {
+            db = new DevelopersBuddyDatabaseDbContext();
+        }
+        public void DeleteUser(int userId)
+        {
+            User existingUser = db.Users.Where(x => x.UserId == userId).FirstOrDefault();
+            if (existingUser != null)
+            {
+                db.Users.Remove(existingUser);
+                db.SaveChanges();
+            }
+        }
+
+        public int GetLatestUserId()
+        {
+            int userId = db.Users.Select(x => x.UserId).Max();
+            return userId;
+        }
+
+        public List<User> GetUsers()
+        {
+            List<User> users = db.Users.Where(x => x.IsAdmin == false).OrderBy(x => x.Name).ToList();
+            return users;
+        }
+
+        public List<User> GetUsersByEmail(string email)
+        {
+            List<User> users = db.Users.Where(x => x.Email == email).ToList();
+            return users;
+        }
+
+        public List<User> GetUsersByEmailAndPassword(string email, string password)
+        {
+            List<User> users = db.Users.Where(x => x.Email==email && x.PasswordHash==password).ToList();
+            return users;
+        }
+
+        public List<User> GetUsersByUserId(int userId)
+        {
+            List<User> users = db.Users.Where(x => x.UserId==userId).ToList();
+            return users;
+        }
+
+        public void InsertUser(User user)
+        {
+            db.Users.Add(user);
+            db.SaveChanges();
+        }
+
+        public void UpdateUserDetails(User user)
+        {
+            User existingUser = db.Users.Where(x => x.UserId == user.UserId).FirstOrDefault();
+            if (existingUser != null)
+            {
+                existingUser.Name = user.Name;
+                existingUser.Mobile = user.Mobile;
+                db.SaveChanges();
+            }
+        }
+
+        public void UpdateUserPassword(User user)
+        {
+            User existingUser = db.Users.Where(x => x.UserId == user.UserId).FirstOrDefault();
+            if (existingUser != null)
+            {
+                existingUser.PasswordHash = user.PasswordHash;
+                db.SaveChanges();
+            }
+        }
+    }
+}
