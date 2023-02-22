5aa04ca494871d39c262ca6c8b5f81c904380896
Akash Jana <akashjana2030@gmail.com>
2023-02-22 20:19:58 +0530

    edit user details, edit user password viewmodel addded

---
+++ DevelopersBuddyProject.ViewModel/EditUserDetailsViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevelopersBuddyProject.ViewModel
+{
+    public class EditUserDetailsViewModel
+    {
+        [Required]
+        [RegularExpression(@"(\w+@[a-zA-Z_]+?\[a-zA-Z]{2,6})")]
+        public string Email { get; set; }
+
+        [Required]
+        [RegularExpression(@"^[a-zA-Z]*$")]
+        public string Name { get; set; }
+
+        [Required]
+        public string Mobile { get; set; }
+    }
+}
