19c7fdbb4000dd53c43d6424bf1dff8052638f4a
Akash Jana <akashjana2030@gmail.com>
2023-02-25 20:23:59 +0530

    bundling and minification added(added microsoft.aspnet.web.optimization and BundleConfig.cs)

---
+++ DevelopersBuddy/App_Start/BundleConfig.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+
+namespace DevelopersBuddy
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/Scripts/bootstrap")
+                .Include("~/Scripts/jquery-3.6.3.js", "~/Scripts/umd/popper.js", "~/Scripts/bootstrap.js"));
+            bundles.Add(new ScriptBundle("~/Styles/bootstrap").Include("~/Content/bootstrap.css"));
+            bundles.Add(new ScriptBundle("~/Styles/site").Include("~/Content/Style.css"));
+            BundleTable.EnableOptimizations = true;
+        }
+    }
+}
