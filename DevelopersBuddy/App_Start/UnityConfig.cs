b4f0ff42aedee2747fd1305881399064e24dca57
Akash Jana <akashjana2030@gmail.com>
2023-02-26 13:30:28 +0530

    index view, question partial view added

--- DevelopersBuddy/App_Start/UnityConfig.cs
+++ DevelopersBuddy/App_Start/UnityConfig.cs
@@ -1,4 +1,6 @@
+using DevelopersBuddyProject.ServiceLayer;
 using System.Web.Http;
+using System.Web.Mvc;
 using Unity;
 using Unity.WebApi;
 
@@ -13,7 +15,8 @@ namespace DevelopersBuddy
             // register all your components with the container here
             // it is NOT necessary to register your controllers
             
-            // e.g. container.RegisterType<ITestService, TestService>();
+            container.RegisterType<IQuestionsService, QuestionsService>();
+            DependencyResolver.SetResolver(new Unity.Mvc5.UnityDependencyResolver(container));
             
             GlobalConfiguration.Configuration.DependencyResolver = new UnityDependencyResolver(container);
         }
---
+++ DevelopersBuddy/Controllers/HomeController.cs
@@ -0,0 +1,59 @@
+﻿using DevelopersBuddyProject.ServiceLayer;
+using DevelopersBuddyProject.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StackOverflowProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        readonly IQuestionsService questionService;
+        readonly ICategoriesService categoriesService;
+
+        public HomeController(IQuestionsService qs, ICategoriesService cs)
+        {
+            this.questionService = qs;
+            this.categoriesService = cs;
+        }
+        public ActionResult Index()
+        {
+            List<QuestionViewModel> questions = this.questionService
+                .GetQuestions().Take(10).ToList();
+            return View(questions);
+        }
+        public ActionResult About()
+        {
+            return View();
+        }
+        public ActionResult Contact()
+        {
+            return View();
+        }
+        public ActionResult Categories()
+        {
+            List<CategoryViewModel> categories = this.categoriesService.GetCategories();
+            return View(categories);
+        }
+        [Route("allquestions")]
+        public ActionResult Questions()
+        {
+            List<QuestionViewModel> questions = this.questionService.GetQuestions();
+            return View(questions);
+        }
+        public ActionResult Search(string str)
+        {
+            List<QuestionViewModel> questions
+                = this.questionService.GetQuestions()
+                .Where(temp => temp.QuestionName.ToLower()
+                .Contains(str.ToLower()) || temp.Category.CategoryName.ToLower()
+                .Contains(str.ToLower())).ToList();
+            ViewBag.str = str;
+            return View(questions);
+        }
+    }
+}
+
+
